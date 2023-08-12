6abc62dc7c1b00f2b36295cc9b43a4723df52b32
Noor Ali <nooralib180@gmail.com>
2023-08-12 10:34:32 +0500

    routing done on the app, little bit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
-import Directory from "./components/directory/directory.component";
-const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
+import Home from "./routes/home/home.component";
+import Navigation from "./components/navigation/navigation.component.jsx";
+import { Routes, Route } from "react-router-dom";
 
+const Shop = () => {
   return (
-    <Directory categories={categories} />
+    <div>
+      <h1>This is Shop Page...</h1>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}></Route>
+        <Route path="shop" element={<Shop />}></Route>
+      </Route>
+    </Routes>
   );
 };
 
---
+++ src/components/navigation/navigation.component.jsx
@@ -0,0 +1,27 @@
+
+import { Fragment } from 'react';
+import './navigation.styles.scss';
+import {ReactComponent as CrwnLogo} from '../../assets/logo.svg';
+
+import {Link, Outlet} from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <header className='navigation'>
+                <div className='navigation-logo-container'>
+                    <Link to="/" className='navigation-logo'>
+                        <CrwnLogo />
+                    </Link>
+                </div>
+                <nav className='navigation-links-container'>
+                    <Link to='/shop' className='navigation-link'>shop</Link>
+                    <Link to='/contacts' className='navigation-link'>contacts</Link>
+                </nav>
+            </header>
+            <Outlet/>
+        </Fragment>
+    );
+}
+
+export default Navigation;
