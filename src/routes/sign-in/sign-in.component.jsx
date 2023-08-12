40cd2e9ee1cd6566f757e637ea8c4b496bfbeb89
Noor Ali <nooralib180@gmail.com>
2023-08-12 20:25:14 +0500

    configured firebase (auth and firestore database), with sign in feature

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,13 +1,43 @@
-import './sign-in.scss';
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
+import "./sign-in.scss";
+
+import {
+  //   auth,
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  //   signInWithGoogleRedirect,
+} from "../../utils/firbase/firbase.util";
 
 const SignIn = () => {
-    
-    return (
-        <div>
-            This is sign in page...
-            <button onClick= {()=>{}} >Click to sign in with google.</button>
-        </div>
-    );
-}
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+
+    // will pass the authorized user...
+    const userDocRef = await createUserDocumentFromAuth(response.user);
+  };
+
+  //   useEffect(async () => {
+  //     const response = await getRedirectResult(auth);
+  //     console.log(response);
+  //   }, []);
+
+  //   const logGoogleUserRedirect = async () => {
+  //     const response = await signInWithGoogleRedirect();
+  //     console.log(response);
+  //   };
+
+  return (
+    <div>
+      This is sign in page...
+      {/* <button onClick={logGoogleUserRedirect}>
+        Click to sign in with google redirect
+      </button> */}
+      <button onClick={logGoogleUser}>
+        Click to sign in with google popup
+      </button>
+    </div>
+  );
+};
 
 export default SignIn;
---
+++ src/utils/firbase/firbase.util.jsx
@@ -0,0 +1,59 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbSardoNMjzpvoJi_a00js_duo2Ll5Sjw",
+  authDomain: "crown-clothing-db-52c34.firebaseapp.com",
+  projectId: "crown-clothing-db-52c34",
+  storageBucket: "crown-clothing-db-52c34.appspot.com",
+  messagingSenderId: "369544403079",
+  appId: "1:369544403079:web:586ca65ca1c22af9da0022",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  // we have to see if there is an existing document refrence, //Note: Reference is a special type of object provided by google to firebase
+
+  // doc(database, collection_name, unique identifire: which will distinguish between diff users.)...
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  // get data of the particular document(user), who's document reference is available.
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("An error occured.", error.message);
+    }
+  }
+
+  return userDocRef;
+};
