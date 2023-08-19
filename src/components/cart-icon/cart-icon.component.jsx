46dca0a2db445be2e7d126d958238ec29ed35c8b
Noor Ali <nooralib180@gmail.com>
2023-08-19 12:46:12 +0500

    adding cart context, cart icon with functionality and cart dropdown list

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,10 +1,10 @@
 import "./cart-icon.styles.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
-const CartIcon = ({onClick}) => {
+const CartIcon = ({ onClick }) => {
   return (
     <div className="cart-icon-container" onClick={onClick}>
-      <ShoppingIcon className="shopping-icon"/>
+      <ShoppingIcon className="shopping-icon" />
       <span className="item-count">0</span>
     </div>
   );
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,14 +1,12 @@
-import {createContext, useState} from 'react';
+import { createContext, useState } from "react";
 
 export const CartContext = createContext({
-    isCartOpen: false,
-    setIsCartOpen: () => {}
-})
+  isCartOpen: false,
+  setIsCartOpen: () => {},
+});
 
-export const CartProvider = ({children}) => {
-    const [isCartOpen, setIsCartOpen] = useState(false)
-    const value = {isCartOpen, setIsCartOpen}
-    return(
-        <CartContext.Provider value={value}>{children}</CartContext.Provider>
-    )
-}
+export const CartProvider = ({ children }) => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const value = { isCartOpen, setIsCartOpen };
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
