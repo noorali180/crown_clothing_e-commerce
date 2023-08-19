5da7083d7ed1eb7e2f87e0b1599f857dd12de63e
Noor Ali <nooralib180@gmail.com>
2023-08-19 12:45:45 +0500

    adding cart context, cart icon with functionality and cart dropdown list

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,16 +1,25 @@
-import { Fragment } from "react";
 import "./navigation.styles.scss";
+import { useContext } from "react";
+import { Link, Outlet } from "react-router-dom";
+import { Fragment } from "react";
+
 import { ReactComponent as CrwnLogo } from "../../assets/logo.svg";
 
-import { Link, Outlet } from "react-router-dom";
-
-import { useContext } from "react";
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 
 import { signOutUser } from "../../utils/firbase/firbase.util";
 
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropDown from "../../components/cart-dropdown/cart-dropdown.component";
+
 const Navigation = () => {
   const { currentUser, setCurrentUser } = useContext(UserContext);
+  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+
+  const toggleCartDropDown = () => {
+    setIsCartOpen(!isCartOpen);
+  };
 
   const signOutUserHandler = async () => {
     await signOutUser();
@@ -31,6 +40,7 @@ const Navigation = () => {
           <Link to="/contacts" className="navigation-link">
             contacts
           </Link>
+
           {currentUser ? (
             <span className="navigation-link" onClick={signOutUserHandler}>
               sign out
@@ -40,7 +50,10 @@ const Navigation = () => {
               sign in
             </Link>
           )}
+
+          <CartIcon onClick={toggleCartDropDown} />
         </nav>
+        {isCartOpen && <CartDropDown />}
       </header>
       <Outlet />
     </Fragment>
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,16 @@
+import "./shop.styles.scss";
+import { useContext } from "react";
+import { ProductsContext } from "../../contexts/products.context";
+import ProductCard from "../../components/product-card/product-card.component";
+
 const Shop = () => {
+  const { products } = useContext(ProductsContext);
+
   return (
-    <div>
-      <h1>This is Shop Page...</h1>
+    <div className="products-container">
+      {products.map((product) => {
+        return <ProductCard key={product.id} product={product}/>;
+      })}
     </div>
   );
 };
