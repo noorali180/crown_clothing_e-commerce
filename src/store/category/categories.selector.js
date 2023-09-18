04b81ea7aca8d8f629cf56f2154ecd21a6450725
Noor Ali <nooralib180@gmail.com>
2023-09-18 21:53:33 +0500

    modifying categories selector

--- src/store/category/categories.selector.js
+++ src/store/category/categories.selector.js
@@ -5,3 +5,9 @@ export const selectCategoriesMap = (state) =>
 
     return acc;
   }, {});
+
+// NOTE: every time selectCategoriesMap function runs, which is a selector function, it will return a new object every single time according to the logic it is build, ------------------ so we have to find a way that it only return a object when the previouse state is changed because useSelector re-renders the component whenever the state is changed in root reducer../
+
+// NOTE: The solution is (Reselect library), it will help us to memoize the previouse state, and will only reutrn an object when state is changed...
+
+// reselct is a library or tool provided in the redux....
